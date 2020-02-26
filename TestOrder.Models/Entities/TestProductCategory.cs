067e75a2614ec08d4fc0bd936a794e521edd9f90
Dmitry Shilov <dmitry@sharp-dev.net>
2020-02-26 11:52:05 +0300

    Text typo

--- TestOrder.Models/Entities/TestProductCategory.cs
+++ TestOrder.Models/Entities/TestProductCategory.cs
@@ -5,7 +5,7 @@ namespace TestOrder.Models.Entities
 {
     public class TestProductCategory
     {
-        //This table haven't Id. Is this a mistake/ambiguity in the task statement?
+        //This table hasn't Id. Is this a mistake/ambiguity in the task statement?
         //In the real situation first of all we need clarify is the relationship between Category and Product one-to-many or many-to-many
         //The current task statement ("into same shipments by address and by product category") implies that a product may belong to only one category
         //So we don't need to use many-to-many and we could remove this table and use one-to-many pattern
